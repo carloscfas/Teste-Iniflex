41f8091af41653b5d23b1b2aeee78cc91fed1b42
carloscfas <Carloasccasd15574@outlook.com>
2026-09-16 14:01:19 -0300

    feature/preparing the FuncionarioService

--- src/service/FuncionarioService.java
+++ src/service/FuncionarioService.java
@@ -1,4 +1,14 @@
 package service;
 
+import model.Funcionario;
+
+import java.util.List;
+
 public class FuncionarioService {
+
+    private List<Funcionario> funcionarios;
+
+    public FuncionarioService(List<Funcionario> funcionarios) {
+        this.funcionarios = funcionarios;
+    }
 }
